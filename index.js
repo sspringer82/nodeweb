fe2e1e172b1bf7a112911e55d25093f8af8473b6
Sebastian Springer <info@sebastian-springer.com>
2017-10-09 10:23:03 +0200

    router

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Express client');
 });
+app.get('/list', (req, res) => {
+  res.send('Hello List');
+});
+app.get('/details', (req, res) => {
+  res.send('Hello Details');
+});
 
 app.listen(8080, () => {
   console.log('listening');
