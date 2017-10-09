35b9a52ee6df6341f755abed3d9dfee6bef9abd9
Sebastian Springer <info@sebastian-springer.com>
2017-10-09 11:46:54 +0200

    ejs

--- controller.js
+++ controller.js
@@ -2,10 +2,11 @@ const model = require('./model');
 
 const controller = {
   listAction(req, res) {
-    res.send(model.getAll());
+    res.render('list');
+    //res.send(model.getAll());
   },
   detailAction(req, res) {
-    const id = parseInt(req.params.id);
+    const id = parseInt(req.params.id, 10);
     res.send(model.getOne(id));
   },
 };
--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const router = require('./router');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(router);
+
+app.use(express.static('public'));
 
 app.listen(8080, () => {
   console.log('Server is listening to http://localhost:8080');
